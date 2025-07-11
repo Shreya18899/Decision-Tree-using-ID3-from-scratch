47248d8cc1d15f500173f31cc5153a6fda62fd65
Shreya18899 <p3h7o6@u.northwestern.edu>
2025-07-10 23:01:05 -0700

    Finished building out the decision tree on examples - run print_tree to see the structure of the tree

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def most_common_label(data, target_col_name):
     counts = {label: labels.count(label) for label in set(labels)}
     return max(counts, key=counts.get)  
 
+def get_categories_column(data, col):
+    values = [row[col] for row in data]
+    counts = {label: values.count(label) for label in set(values)}
+    return values, counts
+
 def check_homogeniety(data, target_col_name):
     flag = False
     labels = [row[target_col_name] for row in data]
@@ -28,8 +33,7 @@ def compute_weighted_child_entropy(attribute):
     weights = {}
     final_entropy_val = []
     print("Attribute being tested is : ", attribute)
-    values = [row[attribute] for row in data]
-    counts = {label: values.count(label) for label in set(values)}
+    values, counts = get_categories_column(data, attribute)
     print(f"Values are \n {values}")
     print(f"Counts are \n {counts}")
 
@@ -54,10 +58,7 @@ def compute_weighted_child_entropy(attribute):
     return sum(final_entropy_val)
 
 def check_homogeniety_attribute_split(data, attribute):
-    values = [row[attribute] for row in data]
-    counts = {label: values.count(label) for label in set(values)}
-    final_label = []
-
+    values, counts = get_categories_column(data, attribute)
     for unique_value in counts.keys():
         print(f"Unique value is {unique_value}")
         # get all the class labels associated with that unique value
@@ -68,19 +69,17 @@ def check_homogeniety_attribute_split(data, attribute):
             print("Homegenous condition not reached")
 
 
-
-def stopping_criteria(data):
+def build_tree(data, attributes):
     # find out the most common label
     cl = most_common_label(data, "Class")
     print(f"Most common label in data is {cl}")
-    # label the node with the most common label
+
+    # label the node with the most common label in the data
     t = node.Node(label=cl)
     print(t.label)
 
     # check the target class
     label, is_homogenous = check_homogeniety(data, "Class")
-    attributes = extract_attributes(data, "Class")
-    print(f"Atttributes are {attributes}")
 
     # If all observations contain only positive or negative observations return
     if is_homogenous == True or not attributes: 
@@ -94,18 +93,57 @@ def stopping_criteria(data):
         return t
     else:
         print("Homogeniety not encountered, building the decision tree further")
-        attribute_list = []
+        attribute_list_tested = []
         entropy_att = []
+        # compute the entropy
         for att in attributes:
-            attribute_list.append(att)
+            attribute_list_tested.append(att)
             entropy = compute_weighted_child_entropy(att)
             entropy_att.append(entropy)
-        att_entropy = dict(zip(attribute_list, entropy_att))
+        att_entropy = dict(zip(attribute_list_tested, entropy_att))
         print(f"Entropy computed for all attributes {att_entropy}")    
         best_attribute = min(att_entropy, key=att_entropy.get)
         print(f"Attribute to split on is {best_attribute}")
-        check_homogeniety_attribute_split(data, best_attribute)
+
+        # create leaf node with the attribute with min entropy which becomes 
+        # the root node
+        root = node.Node(label=best_attribute)
+        values, counts = get_categories_column(data, best_attribute)
+        # children of this node would be the unique values of the attribute
+        for category in counts.keys():
+            subset = [row for row in data if row[best_attribute] == category]
+            # if subset is empty
+            if not subset:
+                # No data for this branch → default to majority class in the data
+                root.children[category] = node.Node(label=cl)
+            else:
+                class_label = [row["Class"] for row in data if row[best_attribute] == category]
+                # if the category contains class labels which are homegenous
+                if len(set(class_label) )== 1:
+                    root.children[category] = node.Node(label=int(next(iter(class_label))))
+                else:
+                    # Implement recursive but only with the subset of data
+                    # Attribute that was split on is getting removed ??
+                    remaining_attrs = [a for a in attributes if a != best_attribute]
+                    child_subtree = build_tree(subset, attributes=remaining_attrs) 
+                    root.children[category] = child_subtree
+        return root
+
+def print_tree(node, depth=0):
+    indent = "  " * depth
+    if node.children:
+        print(f"{indent}[Split on: {node.label}]")
+        for val, child in node.children.items():
+            print(f"{indent}-- {val} -->")
+            print_tree(child, depth + 1)
+    else:
+        print(f"{indent}[Leaf: Predict {node.label}]")
+
 
 if __name__ == "__main__":
     data = parse.parse("candy.data")
-    stopping_criteria(data)
+    attributes = extract_attributes(data, "Class")
+    tree = build_tree(data, attributes=attributes)
+    print(f"Finished building the complete tree \n {tree}")
+
+    print_tree(tree)
